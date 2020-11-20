2be4238445a1624bc27c06c4fd0ca8a27e15a46c
Parminder Reyat <52923589+PSReyat@users.noreply.github.com>
2020-11-20 10:22:54 +0000

    Update shopping-basket.service.ts

--- src/app/Service/shopping-basket/shopping-basket.service.ts
+++ src/app/Service/shopping-basket/shopping-basket.service.ts
@@ -2,18 +2,6 @@ import { Item } from './../../Model/item';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-/*
-1) add decreaseBasket functionality.
-2) functionality including item amount in basket.
-3) reload save state function.
-4) add "thank you for your purchase!" message after the 'pay' button is clicked.
-5) Fix up structure of project.
-   -Code for basket increase and decrease should be part of the service and not the component.
-   -This is so that all the attributes increase in one place and affect everything else as is appropriate.
-   -noOfItems, for example, should only increase in the service and not in the component. This, in turn, will affect the 
-    basket checkout component.
-*/
-
 @Injectable({
   providedIn: 'root'
 })
@@ -117,28 +105,3 @@ export class ShoppingBasketService {
   }
 
 }
-
-
-/*
-save(item: Item): void{
-
-    if(this.noOfItems === 0){
-        this.itemData = new BehaviorSubject(item);
-        this.basketIncrease(item);
-    }else{
-        this.itemData.next(item);
-        this.basketIncrease(item);
-    }
-      
-  }
-
-  basketIncrease(item: Item): void{
-      this.noOfItems++;
-      this.totalPrice += item.price;
-  }
-
-  basketDecrease(item: Item): void{
-      this.noOfItems--;
-      this.totalPrice -= item.price;
-}
-*/
